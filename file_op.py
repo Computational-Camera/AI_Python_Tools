812fbf7ec18264a0c4914434e742631c595b6dd3
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-06-28 16:13:01 +0800

    Add files via upload

---
+++ file_op.py
@@ -0,0 +1,20 @@
+
+
+import glob, os
+import shutil
+
+#copy files
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+try:
+    shutil.copy(src, directory+dst)    
+except:
+    print() 
+
+# search files   
+for root, dirs, files in sorted(os.walk(DATA_DIR)):
+    for f in files:
+        fullpath = os.path.join(root, f)
+            if os.path.splitext(fullpath)[1] == '.json':
+                file_id = f.split('.') 
