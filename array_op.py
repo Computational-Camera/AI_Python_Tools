486d8981019d10c78a3c7789c7c20e4c9863a835
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-06-28 16:16:55 +0800

    Add files via upload

---
+++ array_op.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#find index of arrays that has same tag
+def indices(lst, element):
+    result = []
+    offset = -1
+    while True:
+        try:
+            offset = lst.index(element, offset+1)
+        except ValueError:
+            return result
+        result.append(offset)
+        
---
+++ opencv.py
@@ -0,0 +1,7 @@
+import cv2
+
+
+#contour to bounding box
+cv2.boundingRect(seg)
+#countour to area
+cv2.contourArea (seg)
