bda355e7b73ac8cd41c5895b491cf824309cdcce
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-07-16 13:49:02 +0800

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,8 +1,14 @@
 import cv2
 
+#init an array
+image = np.zeros((height,width,3), np.uint8)
+
 #load image from file
 img = cv2.imread('test.png')# add additional flag 0:for mono import, -1: 16bit
 print(image.shape)
+
+#write image
+cv2.imwrite('img.png', img)
 
 #resize
 img = cv2.resize(img,(y2,x2))
@@ -21,3 +27,5 @@ cv2.rectangle(img, (x1,y1), (x2,y2), (0, 0, 255), 3)
 
 #color coding
 img2 = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+
+
