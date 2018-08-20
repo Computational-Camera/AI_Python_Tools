1ba6dda432466e49b2b4ddcddf16ec57db80649e
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-08-20 12:37:04 +0800

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -20,13 +20,13 @@ img = cv2.resize(img,(w2,h2))
 (b,g,r) = img[x,y]
 
 #contour to bounding box
-cv2.boundingRect(seg)
+cv2.boundingRect(seg)  # [x,y,w,h]
 #countour to area
 cv2.contourArea (seg)
 
 #draw image
 cv2.puText(img, "blabla", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (r,g,b), 2)
-cv2.rectangle(img, (x1,y1), (x2,y2), (0, 0, 255), 3)
+cv2.rectangle(img, (x1,y1), (x2,y2), (0, 0, 255), 3)#, -1 solid
 
 #color coding
 img2 = cv2.applyColorMap(img, cv2.COLORMAP_JET)
