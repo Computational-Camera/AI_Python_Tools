959d00d87d620b78089e8a888581a6ade96d3b80
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-10-01 15:07:14 +0800

    Create pytorch.py

---
+++ pytorch.py
@@ -0,0 +1,6 @@
+
+#image to 4D tensor
+image_tensor = (torch.from_numpy(image).permute(2, 0, 1).contiguous()).unsqueeze(0)
+
+#tensor to cuda tensor
+tensor_gpu = Variable(tesnor_cpu).cuda(async=True)
