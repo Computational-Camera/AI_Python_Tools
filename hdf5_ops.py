20f2368c95b7457ee611c9e42c9708c1aa1f1a68
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-10-12 11:30:27 +0800

    Update hdf5_ops.py

--- hdf5_ops.py
+++ hdf5_ops.py
@@ -4,8 +4,8 @@ import h5py
 data_to_write = np.random.random(size=(100,20))
 
 #=========Write========
-h5py.File('name-of-file.h5', 'w') as hf:
-  hf.create_dataset("name-of-dataset",  data=data_to_write) #must have data=, may also add type="uint16"
+hf = h5py.File('name-of-file.h5', 'w')
+hf.create_dataset("name-of-dataset",  data=data_to_write) #must have data=, may also add type="uint16"
   
 #=========Read=========
 with h5py.File('name-of-file.h5', 'r') as hf:
