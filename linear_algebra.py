4bc46102e583b1cac553ca2b26f2138512c01403
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2020-01-30 11:03:28 +0800

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+#Least Square Fitting, Ax=b, not necessarily linear fitting
+#i.e. quadratic (non linear) fitting by extending the rows of A in the form of x*x, y*y, x*y and constant
 x, err, rank, s= np.linalg.lstsq(A,b)
-
 #still not sure why err does not work
+
+
+#concrete example, 2D plane fitting using normal euqation
+tmp_A = []
+tmp_b = []
+for i in range(len(ptx)):
+    tmp_A.append([ptx[i], pty[i], 1]) # constant coefficient term should be included
+    tmp_b.append(ptz[i])
+b = np.matrix(tmp_b).T
+A = np.matrix(tmp_A)
+fit = (A.T * A).I * A.T * b
+errors = b - A * fit
+residual = np.linalg.norm(errors)
