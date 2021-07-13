2e8390a7afe70209836c66ad6e849b4fa457a4da
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2021-07-13 08:58:32 +0800

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 np.zeros((10,4))
-#check data type
-print(data.dtype)
+#check data type and dimmision
+print(data.dtype, data.dim, data.shape)
 
 #find max or min
 result = np.where(data == np.amax(data)) #amin
